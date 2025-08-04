42f59214fbd4e54faffeb4e3a5cc6c55a421414c
mugeshram <103930695+mugeshram@users.noreply.github.com>
2025-08-04 11:28:34 +0530

    firmware

---
+++ firmware/npk_final.ino
@@ -0,0 +1,145 @@
+#include<SoftwareSerial.h>
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+
+#define RE 11
+#define DE 10
+
+
+const byte inquiry_frame[] = {0x01, 0x03, 0x00, 0x1E, 0x00, 0x03, 0x65, 0xCD};
+const byte inquiry_frame_1[] = {0x01, 0x03, 0x00, 0x1E, 0x00, 0x01, 0xE4, 0x0C};
+const byte inquiry_frame_2[] = {0x01, 0x03, 0x00, 0x1F, 0x00, 0x01, 0xB5, 0xCC};
+const byte inquiry_frame_3[] = {0x01, 0x03, 0x00, 0x02, 0x00, 0x01, 0x85, 0xC0};
+
+byte values[13];
+byte nut[3];
+int command = 0;
+SoftwareSerial mod(5,6);
+SoftwareSerial Bt(2, 3);
+void setup() {
+  Serial.begin(9600);
+  Bt.begin(9600);
+  pinMode(RE,OUTPUT);
+  pinMode(DE,OUTPUT);
+  digitalWrite(RE,LOW);
+  digitalWrite(DE,LOW);
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.setTextSize(3);
+ display.setTextColor(WHITE);
+ display.setCursor(35,8);
+ display.println("IOM"); 
+  display.display();
+
+}
+
+void loop() {
+  display.clearDisplay();
+  delay(250);
+  if(Bt.available()){
+    String r = String(Bt.readString());
+    command = r.toInt();
+  }
+  Serial.println(command);
+  if(command == 1){
+  Bt.end();  
+  mod.begin(9600);  
+  show();
+  read();
+  mod.end();
+  Bt.begin(9600);
+  int n =  nut[0];
+  char ns[16];
+  itoa(n, ns, 10);
+
+  int p =  nut[1];
+  char ps[16];
+  itoa(p, ps, 10);
+
+  int k =  nut[2];
+  char ks[16];
+  itoa(k, ks, 10);
+  char deli[16] = ",";
+  char data[64] = "";
+  strcat(data,ns);
+  strcat(data,deli);
+  strcat(data,ps);
+  strcat(data,deli);
+  strcat(data,ks);
+
+  Serial.println(data);
+  Bt.write(data);
+  Serial.println(nut[0]);
+  Serial.println();
+  delay(1000);
+  }else{
+  display.setTextSize(3);
+ display.setCursor(35,8);
+ display.println("IOM"); 
+  display.display();
+  }
+
+ 
+  
+}
+
+void read(){
+ 
+digitalWrite(DE,HIGH);
+  digitalWrite(RE,HIGH);
+  delay(10);
+  int data = mod.write(inquiry_frame,sizeof(inquiry_frame));
+    digitalWrite(DE,LOW);
+    digitalWrite(RE,LOW);
+    delay(50);
+    if(mod.available()>0){
+    int i=0;
+    int nut_c = 0;
+    
+    while( mod.available()>0){
+    //Serial.print(mod.read(),HEX);
+    values[i] = mod.read();
+    // Serial.println(values[i],HEX);
+    i = i + 1;
+    }
+
+    
+  
+   
+  
+
+    }
+    int length = sizeof(values);
+
+   
+    if(length > 3){
+    for (int n=0;n<length-2;n++){
+        if (values[n] == 1 && values[n+1] == 3 && values[n+2]==6){
+          nut[0] = values[n+4];
+          nut[1] = values[n+6];
+          nut[2] = values[n+8];
+        }
+    }
+    
+    }
+}
+
+
+
+
+void show(){
+
+  display.setTextSize(1);
+  display.setCursor(22, 12);
+  display.print("Calculating...");
+
+
+  display.display();
+}
